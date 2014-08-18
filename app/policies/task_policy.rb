c6dcaec46d3bfcc87d89f7f211999bc67a9ea678
kotaro kimura <kotaro_k@icloud.com>
2014-08-18 09:46:27 +0900

    reset db

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,11 +1,9 @@
 class TaskPolicy < Struct.new(:user, :task)
 
   def hoge?
-  	
   end
 
   def index?
-    false
     #allow_all
   end
 
@@ -22,37 +20,37 @@ class TaskPolicy < Struct.new(:user, :task)
   end
 
   def update?
-	allow_owner_admin
+	  allow_owner_admin
   end
 
   def edit?
-	allow_owner_admin
+	  allow_owner_admin
   end
 
   def destroy?
-	allow_owner_admin
+	  allow_owner_admin
   end
 
   def owned?
-	task.created_user_id === user.id
+	  task.created_user_id === user.id
   end
 
   private
     def is_admin
       user.role === "admin"
-	end
-
-	def owned
-	  task.created_user_id === user.id
-	end
-
-	def allow_all
-	  true
-	end
-
-	def allow_owner_admin
-	  if owned or is_admin
-		true
 	  end
-	end
+
+	  def owned
+	    task.created_user_id === user.id
+	  end
+
+	  def allow_all
+	    true
+	  end
+
+	  def allow_owner_admin
+	    if owned or is_admin
+		    true
+	    end
+	  end
 end
